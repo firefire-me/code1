3e0fa5751e977e7e345581beb22d56b3b395abb1
firefire-me <1550282237@qq.com>
2022-10-19 21:45:15 +0800

    这是一个h2测试用的.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,17 @@ import styles from './index.less';
 // todo:1231
 // [zxy]:4564
 
-// todo:123456
-
 // bug:123
-
-
 
 
 export default function IndexPage() {
   return (
     <div>
+
       <h1 className={styles.title}>Page index</h1>
+      <h1 className={styles.title}>Page index</h1>
+      <h2 className={styles.title}>这是一个h2 标签</h2>
+
     </div>
   );
 }
