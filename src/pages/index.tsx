872bac446b8c45f5e3727cad8d096140a66712fe
firefire-me <1550282237@qq.com>
2022-10-19 21:29:28 +0800

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import styles from './index.less';
+
+
+// todo:1231
+// [zxy]:4564
+
+// todo:123456
+
+// bug:123
+
+
+
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
